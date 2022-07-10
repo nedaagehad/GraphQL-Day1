a552aad1414d1f97c6e5e5e918421ad287b8ea71
Nedaa Gehad <nedaagehad@gmail.com>
2022-07-10 18:20:00 +0200

    Task 2

--- index.js
+++ index.js
@@ -1,81 +1,30 @@
+require("dotenv/config");
 const { ApolloServer, gql } = require("apollo-server");
+const fs = require("fs");
+const path = require("path");
+const resolvers = require("./resolvers/index");
+const mongoose = require("mongoose");
+const PostDataSource = require("./dataSource/post.datasource");
+const CommentDataSource = require("./dataSource/comment.datasource");
 
-const typeDefs = gql`
-  type Post {
-    id: ID!
-    title: String
-    author: String
-  }
+mongoose.connect(process.env.MONGO_CONNECTION_STRING);
 
-  type Query {
-    posts: [Post]
-  }
-
-  type Mutation {
-    create_Post(id: ID!, title: String, author: String): [Post]
-    update_Post(id: ID!, title: String, author: String): [Post]
-    delete_Post(id: ID!): [Post]
-  }
-`;
-
-let posts = [
-  {
-    id: 1,
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    id: 2,
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
-  {
-    id: 3,
-    title: "Animal Farm",
-    author: "George Orwell",
-  },
-];
-
-const resolvers = {
-  Query: {
-    posts: () => posts,
-  },
-
-  Mutation: {
-    create_Post: (_, { id, title, author }) => {
-      let newPost = { id, title, author };
-      posts.push(newPost);
-      return posts;
-    },
-
-    update_Post: (_, { id, title, author }) => {
-      let editPost = posts.map((Post) => {
-        if (Post.id == id) {
-          Post.title = title;
-          Post.author = author;
-        }
-
-        return Post;
-      });
-
-      return editPost;
-    },
-
-    delete_Post: (_, { id }) => {
-      posts = posts.filter((post) => post.id != id);
-      return posts;
-    },
-  },
-};
+const schema = fs.readFileSync(path.join(__dirname, "/schema.graphql"), "utf8");
+const typeDefs = gql(schema);
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   csrfPrevention: true,
   cache: "bounded",
+  dataSources: () => {
+    return {
+      post: new PostDataSource(),
+      comment: new CommentDataSource()
+    }
+  },
 });
 
-// The `listen` method launches a web server.
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
